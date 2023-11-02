7af8b59de5beac0c049e45ad86d6e8d7a491ddf6
julianeon <julianeon@gmail.com>
2023-11-01 20:58:34 -0700

    python scripts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# useful-python-scripts
+# Useful Python Scripts
+
+## make_gif.py
+
+This script makes the images for a gif using imagemagick (must be installed).
+
+The text to be used, and the filenames used for output, are listed in the script.
+
+Run it like this:
+
+python make_gif.py
---
+++ make_gif.py
@@ -0,0 +1,44 @@
+import subprocess
+
+def make_image(text, output_filename, cbackground, cforeground):
+    # Set the background color, text color, and font
+    background = cbackground
+    text_color = cforeground
+    font = "Helvetica"
+    font_size = 48
+
+    # Create a command to generate the image in-memory
+    convert_command = [
+        "convert",
+        "-size", "1600x900",
+        "-background", background,
+        "-fill", text_color,
+        "-font", font,
+        "-pointsize", str(font_size),
+        "-gravity", "center",
+        f"label:{text}",
+        output_filename
+    ]
+
+    # Execute the command to generate the image directly
+    subprocess.run(convert_command)
+
+    print(f"Image created successfully as {output_filename}.")
+
+tarr = (
+    ("Rob Pike's 5 Rules of Programming", "white", "blue"),
+    ("Rob Pike's 5 Rules of Programming", "white", "blue"),
+    ("You can't tell \nwhere a program is going to spend its time.", "black", "white"),
+    ("Measure. \nDon't tune for speed until you've measured.", "black", "yellow"),
+    ("Fancy algorithms are slow \nwhen n is small - and n is usually small.", "black", "white"),
+    ("Fancy algorithms are buggier than simple ones.", "black", "white"),
+    ("Data dominates. \n\n\n", "black", "orange"),
+    ("Data dominates. \nIf you've chosen the right data structures, \nand organized things well, \nthe algorithms will almost always be self-evident.", "black", "orange"),
+    ("Data dominates. \nIf you've chosen the right data structures, \nand organized things well, \nthe algorithms will almost always be self-evident.", "black", "orange"),
+)
+
+for i, (string, color_b, color_t) in enumerate(tarr):
+    fname = f"poly_{i}.png"
+    make_image(string, fname, color_b, color_t)
+
+
